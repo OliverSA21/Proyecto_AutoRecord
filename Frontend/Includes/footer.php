bb5fbd44e982219649e9dd2c6f3b4c85502c69f9
Erick Uriel Perez Valenzuela <240485@utxicotepec.edu.mx>
2025-12-13 22:27:59 -0600

    Proyecto_AutoRecord
    add: Se modifico todo lo de HTML

--- Frontend/Includes/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- includes/footer.php -->
-<footer>
-    © <?= date('Y') ?> Auto-Record. Sistema interno de gestión de transporte público.
-</footer>
-
-<script src="assets/js/script2.js"></script>
-</body>
-</html>
